b4c3a04e0312a475d1e7c199c0f1a5f2e55bbfd3
guang <t90158761@gmail.com>
2022-08-31 16:59:03 +0800

    Update for XXX

--- BootstrapCarousel-20220831-2/club_course/main.js
+++ BootstrapCarousel-20220831-2/club_course/main.js
@@ -1,23 +1,4 @@
-/*$(function(){
-    $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
-    var topicCount = topic.length;
-    let millisecsPerDay = 24*60*60*1000;
 
-
-    for(var x=0;x<topicCount;x++){
-        let text = new Date(startDate.getTime()+7*x*millisecsPerDay).toLocaleDateString();
-        const myArray = text.split("/");
-        
-        $("#courseTable").append(
-            "<tr>"+
-            `<td>${x+1}</td>`+
-            `<td>${myArray[1]}/${myArray[2]}</td>`+
-            // `<td>${text.replace("2022/","")}</td>`+
-            `<td>${topic[x]}</td>`+
-            "</tr>");
-    }
-});
-*/
 let thisInput = document.getElementsByTagName("Input")[2];
 var date =$("#Schoolday").val();
 
