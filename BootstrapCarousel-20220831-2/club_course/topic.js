cb03570a9978e9838d3f8d09552234a5da60b1d6
guang <t90158761@gmail.com>
2022-08-31 16:49:53 +0800

    Update for XXX

--- BootstrapCarousel-20220831-2/club_course/topic.js
+++ BootstrapCarousel-20220831-2/club_course/topic.js
@@ -1,4 +1,4 @@
-var topic = [
+/*var topic = [
     "上課",
     "看第三輪的捍衛戰士",
     "上課",
@@ -22,3 +22,35 @@ var startDate = input.Date();
 
 const mArray = startDate.split("-");
 setMonthAndDay(mArray[1],mArray[2]);
+*/
+
+var topic =[
+    "上課",
+    "看第三輪的捍衛戰士",
+    "上課",
+    "繼續睡覺",
+    "上課",
+    "看得到嗎?"
+];
+
+var topic2 =[
+    "尚未開學",
+    "國定假日",
+    "環境準備",
+    "隨機性",
+    "重複性",
+    "運動會"
+];
+var startDate = new Date ();
+
+//設定好活動第一次的日期
+function setMonthAndDay(starMonth,startDay){
+    //JS的月份是0~11，所以這邊-1
+    startDate.setMonth(starMonth-1,startDay);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
+}
+
+//設定第一次活動日期為2月21日
+setMonthAndDay(2,21);
---
+++ BootstrapCarousel-20220831-2/radio/playList.js
@@ -0,0 +1,13 @@
+// YouTube Video ID
+let playList = [
+    'DDO_aCkivxU',
+    'GbOXV8Oxhak',
+    "c6gi6IP6ay4"
+];
+
+// 播放起訖秒數
+let playTime = [
+    [101,105],
+    [144,150],
+    [146,152]
+];
